f8ce266d1dd45caf0cf1c11648fbfeebb7466f99
imed-elhadef <imed_elhadef@hotmail.fr>
2017-02-01 21:31:21 +0100

    define door sensor input and 3 relays output

--- program.h
+++ program.h
@@ -11,8 +11,21 @@
 
 #include <msp430g2553.h>
 
-#define BLINK_LED1()          (P1OUT ^= BIT0)// Toggle P1.0 using exclusive-OR
-#define BLINK_LED2()          (P1OUT ^= BIT6)// Toggle P1.0 using exclusive-OR
+#define BLINK_LED1()         (P1OUT ^= BIT0)// Toggle P1.0 using exclusive-OR
+#define BLINK_LED2()         (P1OUT ^= BIT6)// Toggle P1.0 using exclusive-OR
+
+#define DOOR_IS_LOW()        ((~P3IN) & BIT2)//Pull up
+#define INPUT1_IS_LOW()      ((~P3IN) & BIT3)//Pull up
+
+#define ACTIV_RELAY1()       (P2OUT |= BIT0)
+#define DEACTIV_RELAY1()     (P2OUT &= ~BIT0)
+
+#define ACTIV_RELAY2()       (P2OUT |= BIT1)
+#define DEACTIV_RELAY2()     (P2OUT &= ~BIT1)
+
+#define ACTIV_RELAY3()       (P2OUT |= BIT2)
+#define DEACTIV_RELAY3()     (P2OUT &= ~BIT2)
+
 
 #define LED1_ON()  (P1OUT |= BIT0)
 #define LED1_OFF() (P1OUT &= ~BIT0)
